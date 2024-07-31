78f611fbd42c1c9c5a6e00f0a6ddc6c24eb6d0a0
MerajAli1 <merajali45425@gmail.com>
2024-07-31 01:37:30 -0700

    Integrated Get All Notice Api in Notice Component in Admin Panel

--- src/admin/Notice.jsx
+++ src/admin/Notice.jsx
@@ -4,7 +4,7 @@ import Button from "@mui/material/Button";
 import CssBaseline from "@mui/material/CssBaseline";
 import TextField from "@mui/material/TextField";
 import Box from "@mui/material/Box";
-import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
+import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
 import Typography from "@mui/material/Typography";
 import Container from "@mui/material/Container";
 import { createTheme, ThemeProvider } from "@mui/material/styles";
@@ -12,13 +12,30 @@ import axios from "axios";
 import { baseURL } from "../api/api";
 const defaultTheme = createTheme();
 export default function Notice() {
-  //STATES
+  //STATES FOR NOTICE
   const [date, setDate] = React.useState("");
   const [title, setTitle] = React.useState("");
-  const [schoolName, setSchoolName] = React.useState("");
   const [noticeDetails, setNoticeDetails] = React.useState("");
+  const [allNotice, setAllNotice] = React.useState([]);
 
-  const handleSubmit = async (event) => {
+  //GET ALL NOTICES FUNCTION
+  const getAllNotices = async () => {
+    try {
+      const token = JSON.parse(localStorage.getItem("token"));
+      const res = await axios.get(`${baseURL}/api/v1/admin/notice`, {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      });
+      console.log(res.data, "NOTICES");
+      setAllNotice(res.data);
+    } catch (error) {
+      console.log("error ", error);
+    }
+  };
+
+  //NOTICE SUBMIT FUNCTION
+  const noticeSubmit = async (event) => {
     event.preventDefault();
     const token = JSON.parse(localStorage.getItem("token"));
     console.log(token, "TOKEN");
@@ -27,13 +44,13 @@ export default function Notice() {
         `${baseURL}/api/v1/admin/notice`,
         {
           title,
-          details:noticeDetails,
+          details: noticeDetails,
           date,
         },
         {
-          headers:{
+          headers: {
             Authorization: `Bearer ${token}`,
-          }
+          },
         }
       );
       console.log(res.data, "NOTICE DATA");
@@ -42,72 +59,106 @@ export default function Notice() {
     }
   };
 
+  //USEEFFECT TO GET ALL NOTICES
+  React.useEffect(() => {
+    getAllNotices();
+  }, []);
   return (
-    <ThemeProvider theme={defaultTheme}>
-      <Container component="main" maxWidth="xs">
-        <CssBaseline />
-        <Box
-          sx={{
-            marginTop: 8,
-            display: "flex",
-            flexDirection: "column",
-            alignItems: "center",
-          }}
-        >
-          <Avatar sx={{ m: 1, bgcolor: "secondary.main" }}>
-            <LockOutlinedIcon />
-          </Avatar>
-          <Typography component="h1" variant="h5">
-            CREATE NOTICE
-          </Typography>
-          <Box
-            component="form"
-            onSubmit={handleSubmit}
-            noValidate
-            sx={{ mt: 1 }}
+    <>
+      <div>
+        <p>
+          <Button
+            variant="contained"
+            fullWidth
+            className="btn btn-primary"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#collapseExample"
+            aria-expanded="false"
+            aria-controls="collapseExample"
           >
-            <TextField
-              onChange={(e) => setTitle(e.target.value)}
-              margin="normal"
-              required
-              fullWidth
-              id="title"
-              label="Notice Title"
-              name="title"
-              autoComplete="title"
-              autoFocus
-            />
-           
-            <textarea
-              onChange={(e) => setNoticeDetails(e.target.value)}
-              className="rounded p-2"
-              style={{ borderColor: "lightgrey" }}
-              rows="4"
-              placeholder="  Notice Details*"
-              cols="50"
-              name=""
-              id=""
-            ></textarea>
-            <div>
-              <TextField
-                onChange={(e) => setDate(e.target.value)}
-                type="date"
-                id="date"
-                name="date"
-                fullWidth
-              />
-            </div>
-            <Button
-              type="submit"
-              fullWidth
-              variant="contained"
-              sx={{ mt: 3, mb: 2 }}
+            All Notices <ExpandMoreIcon />
+          </Button>
+        </p>
+        <div className="collapse" id="collapseExample">
+          {allNotice?.map((e,i)=>(
+            <div key={i} className="card card-body">
+                <h3><b>{e.title}</b></h3>
+                <p>{e.details}</p>
+                <p>Date: {e.date}</p>
+              </div>
+          )
+          )}
+        </div>
+      </div>
+
+      <ThemeProvider theme={defaultTheme}>
+        <Container component="main" maxWidth="xs">
+          <CssBaseline />
+          <Box
+            sx={{
+              marginTop: 8,
+              display: "flex",
+              flexDirection: "column",
+              alignItems: "center",
+            }}
+          >
+            <i
+              style={{ fontSize: "100px" }}
+              className="fa-regular fa-clipboard"
+            ></i>
+            <Typography component="h1" variant="h5">
+              CREATE NOTICE
+            </Typography>
+            <Box
+              component="form"
+              onSubmit={noticeSubmit}
+              noValidate
+              sx={{ mt: 1 }}
             >
-              Create Notice
-            </Button>
+              <TextField
+                onChange={(e) => setTitle(e.target.value)}
+                margin="normal"
+                required
+                fullWidth
+                id="title"
+                label="Notice Title"
+                name="title"
+                autoComplete="title"
+                autoFocus
+              />
+
+              <textarea
+                onChange={(e) => setNoticeDetails(e.target.value)}
+                className="rounded p-2"
+                style={{ borderColor: "lightgrey" }}
+                rows="4"
+                placeholder="  Notice Details*"
+                cols="50"
+                name=""
+                id=""
+              ></textarea>
+              <div>
+                <TextField
+                  onChange={(e) => setDate(e.target.value)}
+                  type="date"
+                  id="date"
+                  name="date"
+                  fullWidth
+                />
+              </div>
+              <Button
+                type="submit"
+                fullWidth
+                variant="contained"
+                sx={{ mt: 3, mb: 2 }}
+              >
+                Create Notice
+              </Button>
+            </Box>
           </Box>
-        </Box>
-      </Container>
-    </ThemeProvider>
+        </Container>
+      </ThemeProvider>
+    </>
   );
 }
