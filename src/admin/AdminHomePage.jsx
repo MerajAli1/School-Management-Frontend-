1755acdf848aa24d6d028e58162c02febf75f517
MerajAli1 <merajali45425@gmail.com>
2024-08-06 01:50:53 -0700

    Added All notices block in admin panel home page

--- src/admin/AdminHomePage.jsx
+++ src/admin/AdminHomePage.jsx
@@ -1,61 +1,104 @@
-import React from 'react'
-import { useNavigate } from 'react-router-dom'
+import React from "react";
+import { useNavigate } from "react-router-dom";
 
 const AdminHomePage = () => {
-  const navigate = useNavigate()
+  const navigate = useNavigate();
   return (
     <>
-    <div className="container-fluid">
-      <h1>Admin Dashboard</h1>
-      {/* First Row */}
+      <div className="container-fluid">
+        <h1>Admin Dashboard</h1>
+        {/* First Row */}
+        <div className="row">
+          {/* First Block */}
+          <div
+            onClick={() => {
+              navigate("/dashboard/attendance");
+            }}
+            className="col-md-3 col"
+          >
+            <div className="text-center p-3 rounded mt-4 shadow border">
+              <i
+                style={{ fontSize: "100px" }}
+                className="fa-solid fa-clipboard-user"
+              ></i>
+              <br />
+              Attendance
+            </div>
+          </div>
 
-      <div className="row">
-        
-        {/* First Block */}
-        <div onClick={()=>{
-          navigate("/dashboard/attendance")
-        }} className="col-md-3 col">
-          <div className='text-center p-3 rounded mt-4 shadow border'>
-          <i style={{fontSize:"100px"}} className="fa-solid fa-clipboard-user"></i><br />
-          Attendance
+          {/* Second Block */}
+          <div
+            onClick={() => {
+              navigate("/dashboard/notice");
+            }}
+            className="col-md-3 col"
+          >
+            <div className="text-center p-3 rounded mt-4 shadow border">
+              <i
+                style={{ fontSize: "100px" }}
+                className="fa-regular fa-clipboard"
+              ></i>
+              <br />
+              Notice
+            </div>
+          </div>
+
+          {/* Third Block */}
+          <div
+            onClick={() => {
+              navigate("/dashboard/registerstudent");
+            }}
+            className="col-md-3 col"
+          >
+            <div className="text-center p-3 rounded mt-4 shadow border">
+              <i
+                style={{ fontSize: "100px" }}
+                className="fa-solid fa-id-card"
+              ></i>
+              <br />
+              Register Student
+            </div>
+          </div>
+
+          {/* Fourth Block */}
+          <div
+            onClick={() => {
+              navigate("/dashboard/registerteacher");
+            }}
+            className="col-md-3 col"
+          >
+            <div className="text-center p-3 rounded mt-4 shadow border">
+              <i
+                style={{ fontSize: "100px" }}
+                className="fa-solid fa-chalkboard-user"
+              ></i>
+              <br />
+              Register Teacher
+            </div>
           </div>
         </div>
-
-        {/* Second Block */}
-        <div onClick={()=>{
-          navigate("/dashboard/notice")
-        }} className="col-md-3 col">
-          <div className='text-center p-3 rounded mt-4 shadow border'>
-          <i  style={{fontSize:"100px"}} className="fa-regular fa-clipboard"></i><br />
-          Notice
-          </div>
-        </div>
-
-        {/* Third Block */}
-        <div onClick={()=>{
-          navigate("/dashboard/registerstudent")
-        }} className="col-md-3 col">
-          <div className='text-center p-3 rounded mt-4 shadow border'>
-          <i style={{fontSize:"100px"}} className="fa-solid fa-id-card"></i><br />
-          Register Student
-          </div>
-        </div>
-
-        {/* Fourth Block */}
-        <div onClick={()=>{
-          navigate("/dashboard/registerteacher")
-        }} className="col-md-3 col">
-          <div className='text-center p-3 rounded mt-4 shadow border'>
-          <i style={{fontSize:"100px"}} className="fa-solid fa-chalkboard-user"></i><br />
-          Register Teacher
+        {/* // Second Row */}
+        <div className="row">
+          {/* First Block */}
+          <div
+            onClick={() => {
+              navigate("/dashboard/allNotice");
+            }}
+            className="col-md-3 col"
+          >
+            <div className="text-center p-3 rounded mt-4 shadow border">
+              <i
+                style={{ fontSize: "100px" }}
+                className="fa-regular fa-clipboard"
+              ></i>
+              <br />
+              All Notices
+            </div>
           </div>
         </div>
       </div>
-      
-      
-    </div>
     </>
-  )
-}
+  );
+};
 
-export default AdminHomePage
+export default AdminHomePage;
