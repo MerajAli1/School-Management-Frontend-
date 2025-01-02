655b3847c155b644a56707d511251df4dcd98e57
MerajAli1 <merajali45425@gmail.com>
2025-01-02 00:31:14 -0800

    Design Student dashboard home page

--- src/student/StudentHomePage.jsx
+++ src/student/StudentHomePage.jsx
@@ -1,8 +1,50 @@
 import React from 'react'
+import { useNavigate } from 'react-router-dom';
 
 const StudentHomePage = () => {
+  const navigate = useNavigate();
   return (
-    <div>StudentHomePage</div>
+    <>
+      <div className="container-fluid">
+        <h1>Student Dashboard</h1>
+        {/* First Row */}
+        <div className="row">
+          {/* First Block */}
+          <div
+            onClick={() => {
+              navigate("/dashboard/studentAttendanceReport");
+            }}
+            className="col-md-3 col"
+          >
+            <div className="text-center p-3 rounded mt-4 shadow border">
+              <i
+                style={{ fontSize: "100px" }}
+                className="fa-solid fa-clipboard-user"
+              ></i>
+              <br />
+              Attendance
+            </div>
+          </div>
+
+          {/* Second Block */}
+          <div
+            onClick={() => {
+              navigate("/dashboard/allNotices");
+            }}
+            className="col-md-3 col"
+          >
+            <div className="text-center p-3 rounded mt-4 shadow border">
+              <i
+                style={{ fontSize: "100px" }}
+                className="fa-regular fa-clipboard"
+              ></i>
+              <br />
+              Notice
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
   )
 }
 
